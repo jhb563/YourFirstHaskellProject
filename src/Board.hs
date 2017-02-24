dad17384d8c1d32d93cc4d785167e3f4c0f4728d
jhb563 <jhbowen047@gmail.com>
2017-02-23 21:02:49 -0800

    Added error handling

--- src/Board.hs
+++ src/Board.hs
@@ -17,7 +17,9 @@ elementAtIndex cell (Board arr) = if inRange (bounds arr) cell
   then Just $ arr ! cell
   else Nothing
 
-updateBoardAtIndex :: (Int, Int) -> BoardCell -> Board -> Maybe Board
-updateBoardAtIndex cellIndex cellType (Board arr) = if inRange (bounds arr) cellIndex
-  then Just (Board (arr // [(cellIndex, cellType)]))
-  else Nothing
+updateBoardAtIndex :: (Int, Int) -> BoardCell -> Board -> Either InputError Board
+updateBoardAtIndex cellIndex cellType (Board arr) = if not (inRange (bounds arr) cellIndex)
+  then Left OutOfBoundsError
+  else if arr ! cellIndex /= BlankCell
+    then Left CellIsOccupiedError
+    else Right (Board (arr // [(cellIndex, cellType)]))
