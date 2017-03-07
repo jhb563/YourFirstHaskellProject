06475855b21e4f614c9d43c1e8a66bed2e3222cf
jhb563 <jhbowen047@gmail.com>
2017-03-06 18:44:50 -0800

    Added Computer Move

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,6 +1,8 @@
 module GameLogic where
 
 import Control.Monad.State
+import Data.Array
+import System.Random
 import Text.Read hiding (lift, get)
 
 import Board
@@ -37,3 +39,20 @@ playHumanMove = do
     printFailure = do
       lift $ putStrLn "Please enter two numbers separated by a space."
       processMoveInput
+
+playComputerMove :: StateT Game IO (Int, Int)
+playComputerMove = do
+  currentGame <- get
+  let board@(Board arr) = gameBoard currentGame
+  cell <- currentCell
+  let choices = [i | i <- indices arr, arr ! i /= BlankCell]
+  indexOfChoice <- lift $ getStdRandom (randomR (0, (length choices) - 1))
+  let finalChoice = choices !! indexOfChoice
+  case updateBoardAtIndex finalChoice cell board of
+    Left err -> do
+      lift $ putStrLn "The Computer picked an illegal move..."
+      lift $ print err
+      playComputerMove
+    Right newBoard -> do
+      put (currentGame { gameBoard = newBoard })
+      return finalChoice
